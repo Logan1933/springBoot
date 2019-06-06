dad1b95ea25a0c5a95eb5bc289c581afbac74ce1
guofeng <guofeng@ec.e-chemchina.com>
2019-06-06 14:42:31 +0800

    github commit Test.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * springBoot启动入口
+ */
 @SpringBootApplication
 public class DemoApplication {
 
